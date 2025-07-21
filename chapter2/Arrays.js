db78679dbd59fc3c2ebffd04f4556fdcdbbd79c8
Hana  Aden Abdi <151941284+hanaaden@users.noreply.github.com>
2025-07-21 18:45:18 +0300

    Update methods Arrays.js

--- chapter2/Arrays.js
+++ chapter2/Arrays.js
@@ -16,3 +16,43 @@ arrr[0] = "new value"
 console.log(arrr[0]) // this is will change the value 
 // arrr = ["new " , "value "] 
 //TypeError: Assignment to constant variable.
+
+//create Array 
+cars = ["toyota", "Tesla" , "BMW"]
+console.log(cars)
+console.log(cars[-1])
+
+//the output will be ["Suzuki", "Tesla" , "BMW"]
+cars[0]="Suzuki"
+console.log(cars)
+
+//checking the length
+console.log(cars.length) //3
+//adding value 
+cars.push("Mercedes")
+console.log(cars)
+// adding with index
+cars.splice(2,0,"chevrolet")
+console.log(cars)
+
+// concat
+let arr = [1,2,3]
+let arr1 = [4,5,6]
+let arr2 = arr.concat(arr1)
+console.log(arr2)
+
+//finding 
+let arr8 = [12,10,5,2]
+let findValue2 = arr8.find(e => e === 10);
+let findIndex3 = arr8.indexOf(6, 2);
+console.log(findValue2);
+console.log(findIndex3)
+
+let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+names.sort();
+console.log(names)
+
+
+
+
+
